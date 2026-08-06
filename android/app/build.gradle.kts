fbc1fcb27a68f993474700d40ca301886bf980fd
Yunji Heo <qufldbswl@naver.com>
2026-08-06 15:00:44 +0900

    카카오맵 혼잡도 지도 작업 시작

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.snob"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36 // flutter.compileSdkVersion 대신 36으로 고정
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,7 +20,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36 // flutter.targetSdkVersion 대신 36으로 고정
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -42,4 +42,13 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+// kakao_map_plugin 등 외부 라이브러리의 compileSdk를 36으로 강제 적용
+gradle.projectsEvaluated {
+    subprojects {
+        extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
+            compileSdkVersion(36)
+        }
+    }
 }
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,26 +1,218 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MapScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+
+
+class MapScreen extends StatefulWidget {
+
   const MapScreen({super.key});
 
 
   @override
-  Widget build(BuildContext context) {
+  State<MapScreen> createState() => _MapScreenState();
+
+}
+
+
+
+class _MapScreenState extends State<MapScreen>{
+
+
+  KakaoMapController? mapController;
+
+
+  List<Polygon> polygons = [];
+
+
+
+  @override
+  void initState(){
+
+    super.initState();
+
+    loadMapData();
+
+  }
+
+
+
+  Future<void> loadMapData() async{
+
+
+    final String data =
+        await rootBundle.loadString(
+          "assets/map/korea_sigungu.geojson"
+        );
+
+
+    final jsonData=jsonDecode(data);
+
+
+
+    List<Polygon> temp=[];
+
+
+
+    for(var feature in jsonData["features"]){
+
+
+      var name =
+          feature["properties"]["SIG_KOR_NM"];
+
+
+
+      // 임시 혼잡도
+      double score =
+          generateScore(name);
+
+
+
+      Color color =
+          getColor(score);
+
+
+
+      var coordinates =
+          feature["geometry"]["coordinates"][0];
+
+
+
+      List<LatLng> points=[];
+
+
+
+      for(var point in coordinates){
+
+        points.add(
+          LatLng(
+            point[1],
+            point[0],
+          )
+        );
+
+      }
+
+
+
+      temp.add(
+
+        Polygon(
+          polygonId:name,
+          points:points,
+          strokeColor:Colors.white,
+          strokeWidth:1,
+          fillColor:color,
+          fillOpacity:0.5,
+        )
+
+      );
+
+
+    }
+
+
+
+    setState((){
+
+      polygons=temp;
+
+    });
+
+
+
+  }
+
+
+
+
+
+  double generateScore(String name){
+
+    // 나중에 SNOB 혼잡도 모델 연결
+
+    return
+        name.hashCode % 100 / 100;
+
+  }
+
+
+
+
+  Color getColor(double score){
+
+
+    if(score <0.33){
+
+      return Colors.green;
+
+    }
+
+    else if(score <0.66){
+
+      return Colors.orange;
+
+    }
+
+    else{
+
+      return Colors.red;
+
+    }
+
+
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context){
+
+
     return Scaffold(
 
-      appBar: AppBar(
-        title: const Text('지도'),
+
+      appBar:AppBar(
+        title:
+        const Text("혼잡도 지도"),
       ),
 
-      body: const Center(
-        child: Text(
-          '지도 화면',
-          style: TextStyle(
-            fontSize: 24,
-          ),
+
+
+      body:KakaoMap(
+
+        onMapCreated:(controller){
+
+          mapController=controller;
+
+        },
+
+
+        center:
+        LatLng(
+          36.5,
+          127.8,
         ),
+
+
+        polygons:polygons,
+
+
+        zoomLevel:13,
+
+
       ),
+
+
 
     );
+
+
   }
+
+
 }
